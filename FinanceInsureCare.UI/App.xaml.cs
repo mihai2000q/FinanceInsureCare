8dd748c0d61a1e06d91b1c423686d7bc5ac2f483
Cris <filipcristimihail@yahoo.com>
2024-12-09 16:40:41 +0100

    UI - Add Prism Application

--- FinanceInsureCare.UI/App.xaml.cs
+++ FinanceInsureCare.UI/App.xaml.cs
@@ -1,9 +1,32 @@
-﻿namespace FinanceInsureCare.UI
+﻿using System.Windows;
+using FinanceInsureCare.Modules.Client;
+using FinanceInsureCare.Modules.Expense;
+using FinanceInsureCare.UI.Views;
+using Prism.DryIoc;
+using Prism.Ioc;
+using Prism.Modularity;
+
+namespace FinanceInsureCare.UI
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App
+    public partial class App : PrismApplication
     {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            moduleCatalog.AddModule<ClientModule>();
+            moduleCatalog.AddModule<ExpenseModule>();
+        }
     }
 }
---
+++ FinanceInsureCare.UI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+
+namespace FinanceInsureCare.UI.ViewModels
+{
+    internal class MainWindowViewModel : BindableBase
+    {
+        private string _title = "Prism Application";
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+
+        public MainWindowViewModel()
+        {
+        }
+    }
+}
