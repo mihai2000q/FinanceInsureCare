14f3143bd0e5190e62f45c56994ebbf31a05fb07
Cris <filipcristimihail@yahoo.com>
2024-12-17 21:45:31 +0100

    Core - Add Enumerable Extensions

---
+++ FinanceInsureCare.Core/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace FinanceInsureCare.Core.Extensions;
+
+public static class EnumerableExtensions
+{
+    public static void ForEach<TSource>(this IEnumerable<TSource> enumerable, Action<TSource> action)
+    {
+        foreach (var source in enumerable) action(source);
+    }
+}
